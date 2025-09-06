b4d56d4a90174ef5bc45d3c0aa53a71136896f17
Seu Nome <seuemail@example.com>
2025-09-05 21:28:43 -0300

    Projeto

---
+++ SistemaAcademico/SistemaAcademico.Repositorio/AlunoRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using SistemaAcademico.Dominio;
+
+namespace SistemaAcademico.Repositorio
+{
+    public class AlunoRepositorio : IAlunoRepositorio
+    {
+        private static List<Aluno> _alunos = new();
+
+        public void Adicionar(Aluno aluno)
+        {
+            _alunos.Add(aluno);
+        }
+
+        public List<Aluno> Listar()
+        {
+            return _alunos;
+        }
+    }
+}
